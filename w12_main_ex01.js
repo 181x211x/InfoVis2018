006837a04a5f43ba1d78f0ff74e18ec39b8fd9f4
高槻　大貴 <181x211x@istc-a-114.epc.center.kobe-u.ac.jp>
2018-05-23 14:38:29 +0900

    w11,12

--- w12_main_ex01.js
+++ w12_main_ex01.js
@@ -15,20 +15,30 @@ function main()
         box.setWidth( 2 );
 
         var seed_point = volume.objectCenter();
-        var streamline = new KVS.Streamline();
-        streamline.setIntegrationStepLength( 0.5 );
-        streamline.setIntegrationTime( 500 );
-        streamline.setIntegrationMethod( KVS.RungeKutta4 );
-        streamline.setIntegrationDirection( KVS.ForwardDirection );
-        streamline.setLineWidth( 5 );
-        streamline.setSeedPoint( seed_point );
+        var streamline1 = new KVS.Streamline();
+        streamline1.setIntegrationStepLength( 11 );
+        streamline1.setIntegrationTime( 20 );
+        streamline1.setIntegrationMethod( KVS.RungeKutta4 );
+        streamline1.setIntegrationDirection( KVS.ForwardDirection );
+        streamline1.setLineWidth( 5 );
+        streamline1.setSeedPoint( seed_point );
+
+        var streamline2 = new KVS.Streamline();
+        streamline2.setIntegrationStepLength( 0.5 );
+        streamline2.setIntegrationTime( 500 );
+        streamline2.setIntegrationMethod( KVS.RungeKutta4 );
+        streamline2.setIntegrationDirection( KVS.ForwardDirection );
+        streamline2.setLineWidth( 5 );
+        streamline2.setSeedPoint( seed_point );
 
         //streamline.setColor(0,255,255);
 
         var line1 = KVS.ToTHREELine( box.exec( volume ) );
-        var line2 = KVS.ToTHREELine( streamline.exec( volume ) );
+        var line2 = KVS.ToTHREELine( streamline1.exec( volume ) );
+        var line3 = KVS.ToTHREELine( streamline2.exec( volume ) );
         screen.scene.add( line1 );
         screen.scene.add( line2 );
+        screen.scene.add( line3 );
         screen.draw();
 
         document.addEventListener( 'mousemove', function() {
